b1e3ebf3b156d4ec3070264962a02bf5f63f3a98
Jérémy Cusin-Mermet <50837004+jrmydix@users.noreply.github.com>
2022-06-24 13:05:34 +0200

    responsive navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,18 +1,33 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
+import burger from "./../public/assets/shared/tablet/icon-hamburger.svg";
 import logo from "./../public/assets/shared/desktop/logo.svg";
 import cart from "./../public/assets/shared/desktop/icon-cart.svg";
 
 const Navigation = () => {
   const router = useRouter();
+  const [isNavActive, setActive] = useState("false");
+  const toggleNav = () => {
+    setActive(!isNavActive);
+  };
 
   return (
     <>
       <header>
-        <Link href="/"><a><Image src={logo} alt="audiophile"></Image></a></Link>
-        <nav>
+        <div className="header__burger" onClick={toggleNav}>
+          <Image src={burger} alt="Open navigation"></Image>
+        </div>
+
+        <Link href="/">
+          <a>
+            <Image src={logo} alt="audiophile"></Image>
+          </a>
+        </Link>
+
+        <nav className={isNavActive ? null : "active"}>
           <ul>
             <li>
               <Link href="/">
@@ -42,6 +57,7 @@ const Navigation = () => {
             </li>
           </ul>
         </nav>
+
         <div className="header__cart">
           <Image src={cart} alt="View cart"></Image>
         </div>
