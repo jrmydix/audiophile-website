a3c2e9a4b329b5ae410eaf325e85363c5d20ad17
Jérémy Cusin-Mermet <50837004+jrmydix@users.noreply.github.com>
2022-06-25 16:28:14 +0200

    responsive footer

--- components/Footer.js
+++ components/Footer.js
@@ -13,57 +13,65 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="footer__left">
-          <Image src={logo} alt="audiophile"></Image>
-
-          <p>
-            Audiophile is an all in one stop to fulfill your audio needs.
-            We&apos;re a small team of music lovers and sound specialists who
-            are devoted to helping you get the most out of personal audio. Come
-            and visit our demo facility - we&apos;re open 7 days a week.
-          </p>
-
-          <p className="footer__left--copyright">
-            Copyright 2022. All Rights Reserved
-          </p>
+        <div className="footer__logo">
+          <Link href="/">
+            <Image src={logo} alt="audiophile"></Image>
+          </Link>
         </div>
 
-        <div className="footer__right">
-          <ul>
-            <li>
-              <Link href="/">
-                <a className={router.pathname == "/" ? "active" : ""}>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/headphones">
-                <a className={router.pathname == "/headphones" ? "active" : ""}>
-                  Headphones
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/speakers">
-                <a className={router.pathname == "/speakers" ? "active" : ""}>
-                  Speakers
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/earphones">
-                <a className={router.pathname == "/earphones" ? "active" : ""}>
-                  Earphones
-                </a>
-              </Link>
-            </li>
-          </ul>
+        <ul>
+          <li>
+            <Link href="/">
+              <a className={router.pathname == "/" ? "active" : ""}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/headphones">
+              <a className={router.pathname == "/headphones" ? "active" : ""}>
+                Headphones
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/speakers">
+              <a className={router.pathname == "/speakers" ? "active" : ""}>
+                Speakers
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/earphones">
+              <a className={router.pathname == "/earphones" ? "active" : ""}>
+                Earphones
+              </a>
+            </Link>
+          </li>
+        </ul>
 
-          <div className="footer__right--socials">
-            <Image src={logoFacebook} alt="Follow us on Facebook"></Image>
+        <p>
+          Audiophile is an all in one stop to fulfill your audio needs.
+          We&apos;re a small team of music lovers and sound specialists who are
+          devoted to helping you get the most out of personal audio. Come and
+          visit our demo facility - we&apos;re open 7 days a week.
+        </p>
 
-            <Image src={logoTwitter} alt="Follow us on Twitter"></Image>
+        <div className="footer__bottom">
+          <p className="footer__copyright">
+            Copyright 2022. All Rights Reserved
+          </p>
 
-            <Image src={logoInstagram} alt="Follow us on Instagram"></Image>
+          <div className="footer__socials">
+            <Link href="/">
+              <Image src={logoFacebook} alt="Follow us on Facebook"></Image>
+            </Link>
+
+            <Link href="/">
+              <Image src={logoTwitter} alt="Follow us on Twitter"></Image>
+            </Link>
+
+            <Link href="/">
+              <Image src={logoInstagram} alt="Follow us on Instagram"></Image>
+            </Link>
           </div>
         </div>
       </footer>
