0c76966a77d0380dd6360e83aec12623a7d0e936
Jérémy Cusin-Mermet <50837004+jrmydix@users.noreply.github.com>
2022-07-12 21:52:53 +0200

    loading bar on page change

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "../styles/styles.scss";
 import Layout from "../components/Layout";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
